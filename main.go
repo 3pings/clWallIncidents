f5d0000a0898e380770667ced78ed15c64e7385e
jbarksdale@threepings.com <jbarksdale@threepings.com>
2020-01-09 07:47:58 -0500

    Added loop to repeat

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/3pings/clWallIncidents/config"
 	"github.com/3pings/clWallIncidents/incidents"
@@ -32,27 +33,27 @@ func main() {
 		log.SetOutput(logwriter)
 	}
 
-	q := incidents.GetIncident(loc, authKey)
-	//Parse fields for specific information
-	id := incidentData{}
+	for {
 
-	id.Description = q.ResourceSets[0].Resources[0].Description
-	id.Severity = q.ResourceSets[0].Resources[0].Severity
-	id.IncidentID = q.ResourceSets[0].Resources[0].IncidentID
-	id.Coordinates = fmt.Sprintf("%f", q.ResourceSets[0].Resources[0].Point.Coordinates[0]) + "," + fmt.Sprintf("%f",q.ResourceSets[0].Resources[0].Point.Coordinates[1])
-	//id.End = strings.SplitAfter(q.ResourceSets[0].Resources[0].End,")")[:1]
-	id.End = strings.Split(strings.Split(q.ResourceSets[0].Resources[0].End, "(")[1], ")")[0][0:10]
+		q := incidents.GetIncident(loc, authKey)
+		//Parse fields for specific information
+		id := incidentData{}
 
-	d := insertData(config.DB, id)
-	if d != nil {
-		fmt.Println(d)
+		id.Description = q.ResourceSets[0].Resources[0].Description
+		id.Severity = q.ResourceSets[0].Resources[0].Severity
+		id.IncidentID = q.ResourceSets[0].Resources[0].IncidentID
+		id.Coordinates = fmt.Sprintf("%f", q.ResourceSets[0].Resources[0].Point.Coordinates[0]) + "," + fmt.Sprintf("%f", q.ResourceSets[0].Resources[0].Point.Coordinates[1])
+		//id.End = strings.SplitAfter(q.ResourceSets[0].Resources[0].End,")")[:1]
+		id.End = strings.Split(strings.Split(q.ResourceSets[0].Resources[0].End, "(")[1], ")")[0][0:10]
 
+		d := insertData(config.DB, id)
+		if d != nil {
+			fmt.Println(d)
+
+		}
+		time.Sleep(120 * time.Second)
 	}
 
-	fmt.Println(id.Coordinates)
-	fmt.Println(id.IncidentID)
-	fmt.Println(id.Severity)
-	fmt.Println(id.End)
 }
 
 //noinspection ALL
